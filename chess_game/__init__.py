b97c1a01a172aef46a41419dacd1a6d4485256cf
Utkrisht Sinha <sinha.utkrisht1997@gmail.com>
2022-01-01 14:41:20 +0530

    Added squares_in_board to init.py

--- chess_game/__init__.py
+++ chess_game/__init__.py
@@ -1,1 +1,2 @@
-from chess_game.chess_game import Board, Player, WHITE, BLACK, InvalidMove, InvalidFen, InvalidBoard, Piece
+from chess_game.chess_game import Board, Player, WHITE, BLACK, InvalidMove, InvalidFen, InvalidBoard, Piece, \
+    squares_in_board
